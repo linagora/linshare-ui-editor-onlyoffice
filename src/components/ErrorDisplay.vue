a7a7d1c0b819f66084aa3d6fd329f13e2ba13bdc
Dung Dung PHAM <dtpham@linagora.com>
2019-03-26 16:47:02 +0700

    linagora/lgs/linshare/products/linshare-editor-onlyoffice#27 Display document not found and user permission errors

--- src/components/ErrorDisplay.vue
+++ src/components/ErrorDisplay.vue
@@ -2,11 +2,18 @@
   <div class="error-display" v-if="errorType">
     <h1>{{$t("Oops!")}}</h1>
     <span v-if="errorType === 'extension'">{{$t("Document file format is not supported")}}</span>
-    <span v-if="errorType === 'connect'">{{$t("Something went wrong on our side. Please try again later.")}}</span>
+    <span v-else-if="errorType === 'connect'">{{$t("Something went wrong on our side. Please try again later.")}}</span>
+    <span v-else-if="errorType === 'document:not-found'">{{$t("Document not found")}}</span>
+    <span v-else-if="errorType === 'user:forbidden'">{{$t("You do not have required permission to edit the document")}}</span>
+    <span v-else-if="errorType === 'user:not-member'">{{$t("You are not a member of the target shared space")}}</span>
     <span v-else>{{$t("Unable to process the document")}}</span>
     <div class="actions">
-      <v-btn v-if="errorType === 'extension'" depressed color="primary" v-bind:href="linshareUrl">{{$t("Back to LinShare")}}</v-btn>
-      <v-btn v-else depressed color="primary" @click="refresh()">{{$t("Refresh")}}</v-btn>
+      <v-btn v-if="action === 'backToLinShare'" depressed color="primary" v-bind:href="linshareUrl">
+        {{$t("Back to LinShare")}}
+      </v-btn>
+      <v-btn v-if="action === 'refresh'" depressed color="primary" @click="refresh()">
+        {{$t("Refresh")}}
+      </v-btn>
     </div>
   </div>
 </template>
@@ -26,6 +33,17 @@ export default {
     refresh: function() {
       this.$router.go();
     }
+  },
+  created: function() {
+    const actionFor = {
+      connect: "refresh",
+      extension: "backToLinShare",
+      "document:not-found": "backToLinShare",
+      "user:forbidden": "backToLinShare",
+      "user:not-member": "backToLinShare"
+    };
+
+    this.action = actionFor[this.errorType];
   }
 };
 </script>
--- src/services/utils.js
+++ src/services/utils.js
@@ -5,5 +5,17 @@ function getErrorType(payload) {
     return "extension";
   }
 
+  if (payload.code === 404 && payload.details && payload.details.match(/Document not found/)) {
+    return "document:not-found";
+  }
+
+  if (payload.code === 404 && payload.details && payload.details.match(/Unable to find user in target workgroup/)) {
+    return "user:not-member";
+  }
+
+  if (payload.code === 403) {
+    return "user:forbidden";
+  }
+
   return "unknown";
 }
