fa2e186ef8acbec275eaa97762102c3adcc5274f
Thang NGUYEN MANH <tnmanh@linagora.com>
2019-07-09 15:23:27 +0700

    esn#2763: add setup, translation for Russian

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -4,6 +4,7 @@ import { FALLBACK_LANGUAGE, DEFAULT_LANGUAGE } from "./constants";
 import en from "@/i18n/lang/en.json";
 import fr from "@/i18n/lang/fr.json";
 import vi from "@/i18n/lang/vi.json";
+import ru from "@/i18n/lang/ru.json";
 
 Vue.use(VueI18n);
 
@@ -13,5 +14,5 @@ const locale = browserLocale.includes("-") ? browserLocale.split("-")[0] : brows
 export const i18n = new VueI18n({
   locale: locale,
   fallbackLocale: FALLBACK_LANGUAGE,
-  messages: { fr, en, vi }
+  messages: { fr, en, vi, ru }
 });
